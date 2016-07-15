d63a3343865fe27b436cb28b69e1a4db5fc43b36
sr <skyrousse@gmail.com>
2016-07-15 10:14:32 -0700

    add passing specs to check client object attributes, also add method to compare Client objects to see if they are teh same object by comparing attributes along with a passing spec

--- lib/client.rb
+++ lib/client.rb
@@ -7,4 +7,8 @@ class Client
     @gender = attributes.fetch(:gender)
     @phone_number = attributes.fetch(:phone_number)
   end
+
+  define_method(:==) do |another_client|
+    self.id().==(another_client.id()).&(self.name().==(another_client.name())).&(self.gender().==(another_client.gender())).&(self.phone_number().==(another_client.phone_number()))
+  end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -7,4 +7,29 @@ describe(Client) do
       expect(test_client.name()).to(eq("Eelin"))
     end
   end
+
+  describe('#gender') do
+    it('returns the gender of the client') do
+    test_client = Client.new({:id => 2, :name => "Eelin", :gender => "Female", :phone_number => 503-345-7898})
+    expect(test_client.gender()).to(eq("Female"))
+    end
+  end
+
+  describe('#phone_number') do
+    it('returns the phone_number of the client') do
+      test_client = Client.new({:id => 2, :name => "Eelin", :gender => "Female", :phone_number => 503-345-7898})
+      expect(test_client.phone_number()).to(eq(503-345-7898))
+    end
+  end
+
+  describe('#==') do
+    it('compares object attributes to determine if they are the same object') do
+      test_client = Client.new({:id => nil, :name => "Eelin", :gender => "Female", :phone_number => 503-345-7898})
+      test_client2 = Client.new({:id => nil, :name => "Eelin", :gender => "Female", :phone_number => 503-345-7898})
+      expect(test_client).to(eq(test_client2))
+    end
+  end
+  # describe('#id') do
+  #   it('returns the id of the client')
+  # end
 end
