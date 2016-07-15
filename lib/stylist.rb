f80f04c549b73d66a6488e131ec7eded0640dcee
sr <skyrousse@gmail.com>
2016-07-15 13:08:25 -0700

    add methods for returning all Stylist objects and saving along with passing specs

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -2,13 +2,30 @@ class Stylist
   attr_reader(:id, :name, :specialty)
 
   define_method(:initialize) do |attributes|
-    @id = attributes[id]
+    @id = attributes[:id]
     @name = attributes.fetch(:name)
     @specialty = attributes.fetch(:specialty)
   end
 
-  define_method('==') do |another_stylist|
+  define_method(:==) do |another_stylist|
     self.id().==(another_stylist.id()).&(self.name().==(another_stylist.name())).&(self.specialty().==(another_stylist.specialty()))
+  end
+
+  define_singleton_method(:all) do
+    stylists = []
+    returned_stylists = DB.exec("SELECT * FROM stylists ORDER BY name ASC;")
+    returned_stylists.each() do |stylist|
+      id = stylist.fetch("id").to_i()
+      name = stylist.fetch("name")
+      specialty = stylist.fetch("specialty")
+      stylists.push(Stylist.new({:id => id, :name => name, :specialty => specialty}))
+    end
+    stylists
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO stylists (name, specialty) VALUES ('#{@name}', '#{@specialty}') RETURNING id;")
+    @id = result.first().fetch("id").to_i()
   end
 
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -23,4 +23,24 @@ describe(Stylist) do
     end
   end
 
+  describe('.all') do
+    it('initially returns an empty array') do
+      expect(Stylist.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves the object to the database') do
+      test_stylist = Stylist.new({:id => nil, :name => "Grace", :specialty => "Cosmetologist"})
+      test_stylist.save()
+      expect(Stylist.all()).to(eq([test_stylist]))
+    end
+  end
+
+  describe('#id') do
+    it('returns a Stylist object by its id') do
+      test_stylist2 = Stylist.new({:id => nil, :name => "Alexis", :specialty => "Esthetician"}).save()
+      expect(test_stylist2).to(be_an_instance_of(Fixnum))
+    end
+  end
 end
