3262d93b072e0dd2091097374a5de4228821bf77
sr <skyrousse@gmail.com>
2016-07-15 18:06:34 -0700

    final commit, there are bugs that needed sorted out for routes and updating and deleting objects

--- app.rb
+++ app.rb
@@ -31,12 +31,17 @@ get('/stylists/:id/edit') do
 end
 
 post('/clients') do
-  stylist_id = params.fetch('')
+  stylist_id = params.fetch('stylist_id')
   name = params.fetch('name')
   gender = params.fetch('gender')
   phone_number = params.fetch('phone_number')
   @stylist = Stylist.find(stylist_id)
-  @client = Client.new({:name => name, :gender => gender, phone_number => phone_number, :stylist_id => stylist_id})
+  @client = Client.new({:name => name, :gender => gender, :phone_number => phone_number, :stylist_id => stylist_id})
   @client.save()
+  erb(:stylist)
+end
+
+get('/stylist/:id') do
+  @stylist= Stylist.find(params.fetch("id").to_i())
   erb(:stylist)
 end
